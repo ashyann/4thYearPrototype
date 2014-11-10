a31fb4cb2a5e921fe6d9e730159b4fd30b6fe82e
ashyann <x00089861@ittd.ie>
2014-11-10 22:16:35 +0000

    Initial commit

---
+++ app/src/main/java/com/ittdfyproject/ashleenrath/collegeplannerprototype/WelcomeActivity.java
@@ -0,0 +1,51 @@
+package com.ittdfyproject.ashleenrath.collegeplannerprototype;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_welcome, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonOnClick(View v) {
+
+        //Do something when login is clicked
+        Button loginbutton = (Button) v;
+        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+
+
+    }
+}
